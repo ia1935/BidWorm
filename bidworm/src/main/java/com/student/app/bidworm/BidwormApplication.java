4b9d4df12f0ca11b89d455d57a80534b18c925db
Imran Ahmed <imranhahmed0987@gmail.com>
2025-02-11 22:52:32 -0500

    changes

--- bidworm/src/main/java/com/student/app/bidworm/BidwormApplication.java
+++ bidworm/src/main/java/com/student/app/bidworm/BidwormApplication.java
@@ -1,5 +1,6 @@
 package com.student.app.bidworm;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
@@ -9,11 +10,13 @@ import java.io.InputStream;
 import java.util.Properties;
 
 @SpringBootApplication
-@PropertySource("classpath:env.properties")
 public class BidwormApplication {
 
 
 	public static void main(String[] args) {
+		//valuable info loaded in. place in root directory
+		Dotenv dotenv = Dotenv.load();
+
 
 		SpringApplication.run(BidwormApplication.class, args);
 	}
