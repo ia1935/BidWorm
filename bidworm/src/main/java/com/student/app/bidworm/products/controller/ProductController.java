4920a59b3f230633ccbb9bf8dd4d09f626979eb8
Imran Ahmed <imranhahmed0987@gmail.com>
2025-02-14 20:54:10 -0500

    posting model

---
+++ bidworm/src/main/java/com/student/app/bidworm/products/controller/ProductController.java
@@ -0,0 +1,4 @@
+package com.student.app.bidworm.products.controller;
+
+public class ProductController {
+}
--- bidworm/src/main/java/com/student/app/bidworm/products/model/Products.java
+++ bidworm/src/main/java/com/student/app/bidworm/products/model/Products.java
@@ -1,4 +1,52 @@
 package com.student.app.bidworm.products.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@Table(name = "product_listings")
 public class Products {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private String postid;
+
+
+    @Column(name = "seller_id")
+    private String sellerId;
+
+    @Column(name = "title",nullable = false)
+    private String title;
+
+    private String description;
+
+    private double price;
+//    private String category;
+
+    @Column(name = "photos")
+    private String photos;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+    
+
+    public Products(){}
+
+
+
 }
